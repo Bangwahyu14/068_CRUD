cdef4dfc96af3a426e57e57f4bd7b23ea61bd4b6
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-10-20 09:46:38 +0700

    menambahkan app get dan app listen

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.get('/', (req,res) => {
+    res.send('Hello world');
+});
 
+app.listen(port, () => {
+    console.log('server is running on port ${port}');
+});
 
